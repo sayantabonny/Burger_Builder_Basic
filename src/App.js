afad58a007dd295af6c584f8ade2bcb429619ad5
sayantabonny <sayantabonny@gmail.com>
2021-03-20 14:13:49 +0530

    Func vs Class Based

--- src/App.js
+++ src/App.js
@@ -11,3 +11,21 @@ function App() {
 }
 
 export default App;
+
+
+/*
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Layout>
+       <h1>Testing</h1>  
+       </Layout>
+       </div>
+    )
+  };
+}
+
+export default App;
+
+*/
